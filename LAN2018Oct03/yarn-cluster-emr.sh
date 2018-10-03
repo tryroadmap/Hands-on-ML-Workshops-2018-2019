52677948f39d9fcc3336730251b565d32b7f71bb
Hoofar Pourzand <support@lotustech.io>
2018-10-03 09:21:07 -0400

    demo update

--- LAN2018Oct03/yarn-cluster-emr.sh
+++ LAN2018Oct03/yarn-cluster-emr.sh
@@ -50,3 +50,108 @@ wget -O /tmp/airlines.csv http://www.transtats.bts.gov/Download_Lookup.asp?Looku
 
 # Download airports data
 wget -O /tmp/airports.csv https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat
+
+
+# Copy flight data to HDFS
+hadoop fs -mkdir /user/rstudio-user/flights/
+hadoop fs -put /tmp/flights /user/rstudio-user/
+
+# Copy airline data to HDFS
+hadoop fs -mkdir /user/rstudio-user/airlines/
+hadoop fs -put /tmp/airlines.csv /user/rstudio-user/airlines
+
+# Copy airport data to HDFS
+hadoop fs -mkdir /user/rstudio-user/airports/
+hadoop fs -put /tmp/airports.csv /user/rstudio-user/airports
+
+
+# Open Hive prompt
+hive
+
+# Create metadata for flights
+CREATE EXTERNAL TABLE IF NOT EXISTS flights
+(
+year int,
+month int,
+dayofmonth int,
+dayofweek int,
+deptime int,
+crsdeptime int,
+arrtime int,
+crsarrtime int,
+uniquecarrier string,
+flightnum int,
+tailnum string,
+actualelapsedtime int,
+crselapsedtime int,
+airtime string,
+arrdelay int,
+depdelay int,
+origin string,
+dest string,
+distance int,
+taxiin string,
+taxiout string,
+cancelled int,
+cancellationcode string,
+diverted int,
+carrierdelay string,
+weatherdelay string,
+nasdelay string,
+securitydelay string,
+lateaircraftdelay string
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+TBLPROPERTIES("skip.header.line.count"="1");
+
+# Load data into table
+LOAD DATA INPATH '/user/rstudio-user/flights' INTO TABLE flights;
+
+
+# Create metadata for airlines
+CREATE EXTERNAL TABLE IF NOT EXISTS airlines
+(
+Code string,
+Description string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES
+(
+"separatorChar" = '\,',
+"quoteChar"     = '\"'
+)
+STORED AS TEXTFILE
+tblproperties("skip.header.line.count"="1");
+
+# Load data into table
+LOAD DATA INPATH '/user/rstudio-user/airlines' INTO TABLE airlines;
+
+
+# Create metadata for airports
+CREATE EXTERNAL TABLE IF NOT EXISTS airports
+(
+id string,
+name string,
+city string,
+country string,
+faa string,
+icao string,
+lat double,
+lon double,
+alt int,
+tz_offset double,
+dst string,
+tz_name string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES
+(
+"separatorChar" = '\,',
+"quoteChar"     = '\"'
+)
+STORED AS TEXTFILE;
+
+# Load data into table
+LOAD DATA INPATH '/user/rstudio-user/airports' INTO TABLE airports;
